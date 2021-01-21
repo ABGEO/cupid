d41f2f17791325268075e7d0ee759ab8ff26772c
Temuri Takalandze <takalandzet@gmail.com>
2021-01-21 19:43:36 +0400

    :sparkles: Implement redirection if the user is logged-in.

--- app/src/main/java/dev/abgeo/cupid/fragment/EntryPointFragment.kt
+++ app/src/main/java/dev/abgeo/cupid/fragment/EntryPointFragment.kt
@@ -1,15 +1,37 @@
 package dev.abgeo.cupid.fragment
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import androidx.navigation.fragment.findNavController
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import dev.abgeo.cupid.R
 
 class EntryPointFragment : Fragment() {
+    private val TAG = this::class.qualifiedName
+    private lateinit var auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        auth = Firebase.auth
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        auth.currentUser?.let {
+            Log.d(TAG, "onStart: User ${it.email} is signed-in")
+            findNavController().navigate(R.id.action_navEntryPointFragment_to_navHomeFragment)
+        }
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
