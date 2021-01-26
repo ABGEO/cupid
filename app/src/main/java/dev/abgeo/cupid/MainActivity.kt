274423c053045c97a318993be467aedee38db4ef
Temuri Takalandze <takalandzet@gmail.com>
2021-01-26 14:52:24 +0400

    :sparkles: Implement Action bar in some fragments.

--- app/src/main/java/dev/abgeo/cupid/MainActivity.kt
+++ app/src/main/java/dev/abgeo/cupid/MainActivity.kt
@@ -2,10 +2,31 @@ package dev.abgeo.cupid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        findNavController(R.id.nav_host_fragment).addOnDestinationChangedListener { _, destination, _ ->
+            when (destination.id) {
+                R.id.navEditProfileFragment,
+                R.id.navSettingsFragment -> {
+                    supportActionBar?.show()
+                }
+                else -> {
+                    supportActionBar?.hide()
+                }
+            }
+        }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return true
     }
 }
